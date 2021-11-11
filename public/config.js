6f770f0391b82974e147daf038eda61f35012042
寒曦朦 <1017959770@qq.com>
2021-11-11 17:10:21 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -16,16 +16,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm786322663-402cd609baf66832d847ef63',
-    'm786322674-0773b2c046e85a9cd3344aa8',
-    'm786322754-8bd9c8c0e7f6bc59ddc376e7',
-    'm786322758-45dc64ec941e47673f2f5bd1',
-    'm786336367-8105e54df5e10b1654fe5df7',
-    'm786336382-19b8bc68931082ae50ad13fd',
-    'm786344761-38ac7c80cfba3bff6b4ee9b0',
-    'm786442374-d8ecbfbf82ad151ba3cf94f6',
-    'm787511928-e4ce8aad127e7c28d714985b',
-    'm788502517-d91440d32f8ab5200e337433',
+    'm789697693-44a60c14a582cbc3e07fc0c1',
+    'm788139590-6e9c6ec1b159e5eab7bf0f5f',
+    'm788139740-7fd3dc1a41ab44819ad0ecf6',
   ],
 
   // 是否显示监测站点的链接
@@ -40,11 +33,11 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://tjys.now.sh/'
+      url: 'https://mon.hanximeng.com/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/qikaile/uptime-status'
+      text: 'Blog',
+      url: 'https://hanximeng.com/'
     }
   ]
 };
